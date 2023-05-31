db55c56495f4e24507cb7e7abb06651b63031220
Davidson Perez <trosquinalidad132@gmail.com>
2023-05-30 22:56:08 -0500

    Co-authored-by: Jwhitegomez <Jwhitegomez@users.noreply.github.com>

---
+++ src/main/java/com/davidsonperez/other/trabajo2/logyrepii/Pila.java
@@ -0,0 +1,31 @@
+package com.davidsonperez.other.trabajo2.logyrepii;
+
+public class Pila {
+    private Persona[] vectorPersonas;
+    private int tope;
+
+    public Pila(int cont) {
+        vectorPersonas = new Persona[cont];
+        tope = -1;
+    }
+
+    public boolean apilar(Persona persona) {
+        if (tope == vectorPersonas.length - 1) {
+            return false; // La pila está llena
+        }
+        tope++;
+        vectorPersonas[tope] = persona;
+        return true;
+    }
+
+    public Persona desapilar() {
+        Persona persona = vectorPersonas[tope];
+        tope--;
+        return persona;
+    }
+
+    public boolean estaVacia() {
+        return tope == -1;
+    }
+    
+}
--- src/main/java/com/davidsonperez/other/trabajo2/logyrepii/Trabajo2LogYRepII.java
+++ src/main/java/com/davidsonperez/other/trabajo2/logyrepii/Trabajo2LogYRepII.java
@@ -1,8 +1,32 @@
 package com.davidsonperez.other.trabajo2.logyrepii;
 
+import java.util.Scanner;
+
 public class Trabajo2LogYRepII {
 
     public static void main(String[] args) {
-        
+        String nom;
+        char sexo;
+        int edad;
+        Scanner input = new Scanner(System.in);
+        ListaDoblementeLigPers listaPersonas = new ListaDoblementeLigPers();
+
+        System.out.println("Ingrese los datos de la persona (999 en nombre para salir): ");
+        System.out.println("Ingrese los datos de la persona (999 en nombre para salir): ");
+
+        System.out.println("Nombre: ");
+        nom = input.nextLine();
+
+        while (nom != "999") {
+            System.out.println("Sexo: ");
+            sexo = input.next().charAt(0);
+            System.out.println("Edad: ");
+            edad = input.nextInt();
+
+            listaPersonas.ingresar(nom, sexo, edad);
+
+            System.out.println("Nombre: ");
+            nom = input.nextLine();
+        }
     }
 }
